206137b83499f96c00954d18bf063d9a17388a4c
Seongki Kim <moztiq@gmail.com>
2024-01-09 20:27:52 +0900

    Animation Values

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
 class ExplicitAnimationsScreen extends StatefulWidget {
   const ExplicitAnimationsScreen({super.key});
@@ -11,12 +12,32 @@ class ExplicitAnimationsScreen extends StatefulWidget {
 
 class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
     with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController =
-      AnimationController(vsync: this);
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: Duration(seconds: 10),
+  );
+
+  void _play() {
+    _animationController.forward();
+  }
+
+  void _pause() {
+    _animationController.stop();
+  }
+
+  void _rewind() {
+    _animationController.reverse();
+  }
 
   @override
   void initState() {
     super.initState();
+    Timer.periodic(
+      Duration(milliseconds: 500),
+      (timer) {
+        print(_animationController.value);
+      },
+    );
   }
 
   @override
@@ -24,6 +45,37 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
     return Scaffold(
       appBar: AppBar(
         title: Text('Explicit Animations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '${_animationController.value}',
+              style: TextStyle(fontSize: 58),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _play,
+                  child: Text('Play'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _pause,
+                  child: Text('Pause'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _rewind,
+                  child: Text('Rewind'),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
