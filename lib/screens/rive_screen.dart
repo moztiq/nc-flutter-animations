408cd0486c587a4bfdc93c09968110a97dbe7ece
Seongki Kim <moztiq@gmail.com>
2024-01-19 16:57:08 +0900

    custom button animation

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -17,23 +15,18 @@ class _RiveScreenState extends State<RiveScreen> {
       body: Stack(
         children: [
           const RiveAnimation.asset(
-            "assets/animations/balls-animation.riv",
-            fit: BoxFit.cover,
+            "assets/animations/custom-button-animation.riv",
+            stateMachines: ["state"],
           ),
-          Positioned.fill(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(
-                sigmaX: 50,
-                sigmaY: 50,
-              ),
-              child: const Center(
-                child: Text(
-                  'Welcome to AI App',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
-                ),
+          Center(
+            child: Text(
+              'Login',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
               ),
             ),
-          ),
+          )
         ],
       ),
     );
