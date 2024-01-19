64283740a6f4d3c801cf7d8d2e0e7b5e0047f159
Seongki Kim <moztiq@gmail.com>
2024-01-19 16:30:19 +0900

    Animation Blur

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -9,48 +11,30 @@ class RiveScreen extends StatefulWidget {
 }
 
 class _RiveScreenState extends State<RiveScreen> {
-  late StateMachineController _stateMachineController;
-
-  void _onInit(Artboard artboard) {
-    _stateMachineController =
-        StateMachineController.fromArtboard(artboard, "state", onStateChange: (
-      String stateMachineName,
-      String stateName,
-    ) {
-      print(stateMachineName);
-      print(stateName);
-    })!;
-    artboard.addController(_stateMachineController);
-  }
-
-  void _togglePanel() {
-    final input = _stateMachineController.findInput<bool>("panelActive")!;
-    input.change(!input.value);
-  }
-
-  @override
-  void dispose() {
-    _stateMachineController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Rive'),
-      ),
-      body: Center(
-        child: Container(
-          color: Color(0xFFFF2ECC),
-          width: double.infinity,
-          child: RiveAnimation.asset(
-            'assets/animations/stars-animation.riv',
-            artboard: "artboard",
-            stateMachines: ["state"],
-            onInit: _onInit,
+      body: Stack(
+        children: [
+          const RiveAnimation.asset(
+            "assets/animations/balls-animation.riv",
+            fit: BoxFit.cover,
           ),
-        ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 50,
+                sigmaY: 50,
+              ),
+              child: const Center(
+                child: Text(
+                  'Welcome to AI App',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
