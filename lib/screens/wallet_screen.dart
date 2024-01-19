84baf99b3c97afa415a9cf5b576a52ae18207995
Seongki Kim <moztiq@gmail.com>
2024-01-19 20:21:10 +0900

    Credit Cards

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -15,31 +15,95 @@ class _WalletScreenState extends State<WalletScreen> {
       appBar: AppBar(
         title: const Text('Wallet'),
       ),
-      body: Center(
-        child: const Text(
-          'Hello!',
-          style: TextStyle(
-            fontSize: 66,
-          ),
-        )
-            .animate()
-            .fadeIn(
-              begin: 0,
-              duration: 5.seconds,
-              curve: Curves.easeInOutCubic,
-            )
-            .scale(
-              duration: 5.seconds,
-              begin: Offset.zero,
-              end: const Offset(1, 1),
-              curve: Curves.easeInOutCubic,
-            )
-            .then(delay: 2.seconds)
-            .slideX(
-              begin: 0,
-              end: -10,
-              duration: 2.seconds,
+      body: const Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            CreditCard(bgColor: Colors.purple),
+            CreditCard(bgColor: Colors.black),
+            CreditCard(bgColor: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CreditCard extends StatelessWidget {
+  final Color bgColor;
+
+  const CreditCard({super.key, required this.bgColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: bgColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 40,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'SEONGKI KIM',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      '**** **** **75',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Positioned(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 20,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
